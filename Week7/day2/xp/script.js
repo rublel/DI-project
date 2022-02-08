4d89efaaf882cf4d1df38ead53b3d35f004e7e96
Ruben Lellouche <rubenlellouche26@gmail.com>
2022-02-08 14:11:31 +0200

    w7d2ex1

--- Week7/day2/xp/script.js
+++ Week7/day2/xp/script.js
@@ -1,14 +1,18 @@
 //------1------
-const fruits = ["apple", "orange"];
-const vegetables = ["carrot", "potato"];
+const fruits = ["apple", "orange"]; //this is a simple array with 2 elements
+const vegetables = ["carrot", "potato"];//this is a simple array with 2 elements
 
+//destructuration of vegetables array and put it after 'bread'into a new array called results - idem for fruits
 const result = ['bread', ...vegetables, 'chicken', ...fruits];
 console.log(result);
 
 //------2------
+// country is a variable who contains a string with 3 letters
 const country = "USA";
+//generate an array where all letters of country became elements of the array
 console.log([...country]);
 
 //------Bonus------
+//we dont have any values but its the same to write just the array and console log it 
 let newArray = [...[,,]];
 console.log(newArray);
