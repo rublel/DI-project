2dbc8ae7f2a40094a94e08ae7169307e3c198e25
ruben <rublel.dev@gmail.com>
2022-03-14 22:21:16 +0200

    node-js

---
+++ Week12/day2/XP/main.js
@@ -0,0 +1,14 @@
+//Exercise 1
+let a = 5;
+let b = 10;
+console.log('Hello you are ' + (a + b));
+
+//Exercise 2
+console.log('Hi, thanks for running me with npm start!');
+
+//Exercie 3
+let x;
+let c = 6;
+x = a + b;
+console.log('First run: ' + c);
+console.log('Second run: ' + (x + c)); 
